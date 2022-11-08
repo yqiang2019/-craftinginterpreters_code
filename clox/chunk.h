3ca3ccf2098c76f133b4d715112795a9afaa12c0
YAN <416239076@qq.com>
2022-11-08 15:45:48 +0800

    Add files via upload

---
+++ clox/chunk.h
@@ -0,0 +1,59 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+  OP_CONSTANT,
+  OP_EQUAL,
+  OP_GREATER,
+  OP_LESS,
+  OP_NOT,
+  OP_NEGATE,
+  OP_NIL,
+  OP_TRUE,
+  OP_FALSE,
+  OP_ADD,
+  OP_SUBTRACT,
+  OP_MULTIPLY,
+  OP_DIVIDE,
+  OP_GET_PROPERTY,
+  OP_SET_PROPERTY,
+  OP_CLASS,
+  OP_INHERIT,
+  OP_GET_SUPER,
+  OP_SUPER_INVOKE,
+  OP_INVOKE,
+  OP_METHOD,
+  OP_CLOSE_UPVALUE,
+  OP_GET_UPVALUE,
+  OP_SET_UPVALUE,
+  OP_LOOP,
+  OP_CLOSURE,
+  OP_CALL,
+  OP_JUMP_IF_FALSE,
+  OP_JUMP,
+  OP_POP,
+  OP_GET_LOCAL,
+  OP_SET_LOCAL,
+  OP_SET_GLOBAL,
+  OP_GET_GLOBAL,
+  OP_DEFINE_GLOBAL,
+  OP_RETURN,
+  OP_PRINT
+} OpCode;
+
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t* code;
+  int* lines;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
+#endif
